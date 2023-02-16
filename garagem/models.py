1c5eeca36f0d6bed38490b1f161df5b388865c74
Marco André Mendes <marcoandre@gmail.com>
2023-02-16 15:47:17 -0300

    Commit inicial

---
+++ garagem/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Marca(models.Model):
+    """Marca de um veículo, como por exemplo: Fiat, Volkswagen, etc."""
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome.title()
+
+
+class Categoria(models.Model):
+    """Categoria de um veículo, como por exemplo: Carro, Moto, Caminhão, etc."""
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao.title()
