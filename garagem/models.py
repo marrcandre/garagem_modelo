c4d86bc5d4c6e66c0e9d60d80feb4fa842aa382d
Marco André Mendes <marcoandre@gmail.com>
2023-02-16 15:54:13 -0300

    Formatação dos arquivos do projeto

--- garagem/models.py
+++ garagem/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class Marca(models.Model):
     """Marca de um veículo, como por exemplo: Fiat, Volkswagen, etc."""
+
     nome = models.CharField(max_length=50)
 
     def __str__(self):
@@ -10,6 +12,7 @@ class Marca(models.Model):
 
 class Categoria(models.Model):
     """Categoria de um veículo, como por exemplo: Carro, Moto, Caminhão, etc."""
+
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
