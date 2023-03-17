dc6046c2e38ea24ab2f542d60fdd5f898f46569f
Marco André Mendes <marcoandre@gmail.com>
2023-03-17 13:36:37 -0300

    novas entidades

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Categoria, Marca
+from .models import Acessorio, Categoria, Cor, Marca, Veiculo
 
-admin.site.register(Marca)
+admin.site.register(Acessorio)
 admin.site.register(Categoria)
+admin.site.register(Cor)
+admin.site.register(Marca)
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -7,7 +7,7 @@ class Marca(models.Model):
     nome = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.nome.title()
+        return self.nome.upper()
 
 
 class Categoria(models.Model):
@@ -17,3 +17,46 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao.title()
+
+
+class Acessorio(models.Model):
+    """Acessórios de um veículo, como por exemplo: Ar condicionado, Direção hidráulica, etc."""
+
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao.title()
+    
+    class Meta:
+        verbose_name_plural = "acessórios"
+        verbose_name = "acessório"
+
+
+class Cor(models.Model):
+    """Cor de um veículo, como por exemplo: Vermelho, Azul, etc."""
+
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao.title()
+    
+    class Meta:
+        verbose_name_plural = "cores"
+
+
+class Veiculo(models.Model):
+    """Veículo, como por exemplo: Gol, Uno, etc."""
+
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    acessorios = models.ManyToManyField(Acessorio)
+    ano = models.IntegerField(null=True, blank=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0.0)
+
+    def __str__(self):
+        return f"{self.marca} {self.categoria} {self.ano} - {self.cor}"
+
+    class Meta:
+        verbose_name_plural = "veículos"
+        verbose_name = "veículo"
