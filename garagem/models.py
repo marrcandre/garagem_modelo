bb9e600d1f063660a889c8ee3f299175b9ac7bc3
Marco André Mendes <marcoandre@gmail.com>
2023-04-28 10:53:32 -0300

    Inclusão do campo Modelo no Veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -55,9 +55,10 @@ class Veiculo(models.Model):
     preco = models.DecimalField(
         max_digits=10, decimal_places=2, null=True, blank=True, default=0.0
     )
+    modelo = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.marca} {self.categoria} {self.ano} - {self.cor}"
+        return f"{self.marca} {self.modelo} {self.categoria} {self.ano} - {self.cor}"
 
     class Meta:
         verbose_name_plural = "Veículos"
